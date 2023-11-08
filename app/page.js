6ad294b1952660004d6a32186d268ac87d2280f0
it21294198 <it21294198@my.sliit.lk>
2023-11-08 17:31:44 +0530

    add changes 1

--- app/page.js
+++ app/page.js
@@ -16,32 +16,32 @@ export default function Home() {
 
   return (
     <>
-      {loading ? (
+      {/* {loading ? (
         <div className="bg-black h-screen w-screen flex items-center justify-center">
           <SpinnerDotted size="20%" color="#f70c0c" speed="100" />
         </div>
-      ) : (
-        <>
-          <div className="relative text-white">
-            <div className="absolute bg-white bg-opacity-50 left-1/3 p-10">
-              <div className="h-screen overflow-y-scroll p-40 scroll-smooth">
-                {/* main content of the portfolio web site */}
-                <Details />
-                {/* <Projects /> */}
-                {/* <MainImage
+      ) : ( */}
+      <>
+        <div className="relative text-white">
+          <div className="absolute bg-white bg-opacity-50 left-1/3 p-10">
+            <div className="h-screen overflow-y-scroll p-40 scroll-smooth">
+              {/* main content of the portfolio web site */}
+              <Details />
+              {/* <Projects /> */}
+              {/* <MainImage
                   imagURL={"/Giff.gif"}
                   imageSize={500}
                   imageName={"Intro image"}
                   border={"rounded-3xl"}
                 /> */}
-                <br />
-                <SocialMedia />
-              </div>
+              <br />
+              <SocialMedia />
             </div>
-            <Canvas />
           </div>
-        </>
-      )}
+          <Canvas />
+        </div>
+      </>
+      {/* )} */}
     </>
   );
 }
